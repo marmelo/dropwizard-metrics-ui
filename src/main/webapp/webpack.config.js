175f216a058d888438262bffee9916d6e2e7198b
Rafael Marmelo <rmarmelo@gmail.com>
2018-10-09 22:41:25 +0100

    Removed react-scripts in favor of plain babel and webpack.

---
+++ src/main/webapp/webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
+
+module.exports = config;
