da8450b58ebf6902e03312ed1e258fe5549af612
Rafael Marmelo <rmarmelo@gmail.com>
2018-11-13 00:48:46 +0000

    Added overview widgets.

--- src/main/webapp/src/widgets/OverviewWidget.js
+++ src/main/webapp/src/widgets/OverviewWidget.js
@@ -5,19 +5,39 @@ import Row from 'antd/lib/row';
 import Col from 'antd/lib/col';
 
 import prettyMs from 'pretty-ms';
+import prettyBytes from 'pretty-bytes';
 
 import AbstractWidget from './AbstractWidget.js';
 
 class OverviewWidget extends AbstractWidget {
+
+    constructor(props) {
+        super(props);
+        
+        this.requests = 0;
+        this.logs = 0;
+    }
     
     render() {
-        const name = this.getGauge('jvm.attribute.name');
-        
-        const vendor = this.getGauge('jvm.attribute.vendor');
-        
         const uptime = this.getGauge('jvm.attribute.uptime');
         const uptimePretty = uptime ? prettyMs(uptime, { secDecimalDigits: 0 }) : "-";
         
+        const heapUsed = this.getGauge('jvm.memory.heap.used') || -1;
+        const heapUsedPretty = prettyBytes(heapUsed);
+        
+        const heapMax = this.getGauge('jvm.memory.heap.max') || -1;
+        const heapMaxPretty = prettyBytes(heapMax);
+        
+        const requests = this.getTimer('io.dropwizard.jetty.MutableServletContextHandler.requests') || {};
+        const requestsTotal = requests.count;
+        const requestsRate = requestsTotal - this.requests;
+        this.requests = requestsTotal;
+
+        const logs = this.getMeter('ch.qos.logback.core.Appender.all') || {};
+        const logsTotal = logs.count;
+        const logsRate = logsTotal - this.logs;
+        this.logs = logsTotal;
+
         return (
             <Row gutter={16}>
                 <Col span={6}>
@@ -28,20 +48,20 @@ class OverviewWidget extends AbstractWidget {
                 </Col>
                 <Col span={6}>
                     <Card className="dwm-overview">
+                        <span>Heap</span>
+                        {heapUsedPretty} out of {heapMaxPretty}
+                    </Card>
+                </Col>
+                <Col span={6}>
+                    <Card className="dwm-overview">
                         <span>Requests</span>
-                        -
+                        {requestsRate} reqs/s out of {requestsTotal}
                     </Card>
                 </Col>
                 <Col span={6}>
                     <Card className="dwm-overview">
                         <span>Logs</span>
-                        -
-                    </Card>
-                </Col>
-                <Col span={6}>
-                    <Card className="dwm-overview">
-                        <span>Health</span>
-                        -
+                        {logsRate} logs/s out of {logsTotal}
                     </Card>
                 </Col>
             </Row>
